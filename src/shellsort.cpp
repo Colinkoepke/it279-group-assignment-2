c603b9f86d8aa89b76b33badaf4832273a5b00db
Brandon Manke <me@brandonmanke.com>
2017-10-17 17:43:00 -0500

    Made vector of type long instead of ints

--- src/shellsort.cpp
+++ src/shellsort.cpp
@@ -19,10 +19,10 @@ int calculateGap(const int size) {
  * Shell sort using Hibbard's gap
  * Time complexity O(n^1.5)
  */
-void shellsort(std::vector<int> &arr) {
+void shellsort(std::vector<long> &arr) {
     for (int gap = calculateGap(arr.size()); gap > 0; gap /= 2) {
         for (int i = gap; i < arr.size(); ++i) {
-            int temp = arr.at(i);
+            long temp = arr.at(i);
             int j;
             for (j = i; j >= gap && arr.at(j - gap) > temp; j -= gap) {
                 arr.at(j) = arr.at(j - gap);
@@ -30,4 +30,19 @@ void shellsort(std::vector<int> &arr) {
             arr.at(j) = temp;
         }
     }
+}
+
+int main() {
+    std::vector<long> v;
+    v.push_back(33);
+    v.push_back(15);
+    v.push_back(17);
+    v.push_back(3);
+    v.push_back(7);
+    shellsort(v);
+    for (int i = 0; i < v.size(); i++)
+        std::cout << v.at(i) << ",";
+    
+    std::cout << std::endl;
+    return 0;
 }
