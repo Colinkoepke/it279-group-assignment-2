4d639caa5cce493e69d6e2b9123cc5e2a6498b9f
Brandon Manke <manke.brandon@gmail.com>
2017-10-22 16:18:00 -0500

    Finished, reworked due to ambiguous directions

---
+++ README.txt
@@ -0,0 +1,14 @@
+Group Assignment 2
+
+Compile each file seperately each one takes the input file and output file on the command line.
+
+Example:
+
+g++ insertionSort.cpp -o out1
+./out1 "input.txt" "output.txt"
+
+g++ mergeSort.cpp -o out2
+./out2 "input.txt" "output.txt"
+
+g++ shellsort.cpp -o out3
+./out3 "input.txt" "output.txt"
---
+++ shellsort.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include "shellsort.h"
+using namespace std;
+
+/**
+ * Inefficient hibbards gap calculation
+ */
+int calculateGap(const int size) {
+    int gap = 0;
+    int i = 1;
+    while (gap < size) {
+        gap = (2 << i) - 1;
+        i++;
+    }
+    gap /= 2;
+    return gap;
+}
+
+/**
+ * Shell sort using Hibbard's gap
+ * Time complexity O(n^1.5)
+ */
+void shellsort(std::vector<long> &arr) {
+    for (int gap = calculateGap(arr.size()); gap > 0; gap /= 2) {
+        for (int i = gap; i < arr.size(); ++i) {
+            long temp = arr.at(i);
+            int j;
+            for (j = i; j >= gap && arr.at(j - gap) > temp; j -= gap) {
+                arr.at(j) = arr.at(j - gap);
+            }
+            arr.at(j) = temp;
+        }
+    }
+}
+
+void readFile(const string fileName, vector<long> &arr) {
+    ifstream inFile;
+    inFile.open(fileName);
+
+    if(!inFile)
+        cout << "error opening file" << endl;
+    else{
+        long readLong;
+        while(!inFile.eof()){
+            inFile >> readLong;
+            arr.push_back(readLong);
+        }
+    }
+}
+
+void writeFile(const string outFileName, vector<long> &arr) {
+    ofstream outfile (outFileName);
+
+    cout << "List numbers after sorting:" << endl;
+    if (outfile.is_open()){
+        for (int i=0; i < arr.size(); i++){
+            cout << arr.at(i) << endl; // temp
+            outfile << arr.at(i) << endl;
+        }
+    }
+    else
+        cout << "Unable to open file";
+
+    outfile.close();
+}
+
+//main function to call sort and take command line arguments
+int main(int argc, char* argv[]) {
+    vector<long> shellSortVector;
+
+    string filename = argv[1];
+    readFile(filename, shellSortVector);
+
+    //cout << "List of numbers before sorting:" << endl;
+    //for (int i=0; i < mergeSortVector.size(); i++)
+    //    cout << mergeSortVector.at(i) << endl;
+
+    shellsort(shellSortVector);
+
+    /*cout << endl << "After sorting" << endl;
+    for (int i = 0; i < mergeSortVector.size(); i++) {
+        cout << mergeSortVector.at(i) << endl;
+    }
+    //shellsort(mergeSortVector);*/
+
+    string outfilename = argv[2];
+
+    writeFile(outfilename, shellSortVector);
+    
+    return 0;
+}
